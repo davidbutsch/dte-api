b1212c1e2cf85f6627d42d5ebc72cf276ff187f5
David <102764748+davidbutsch@users.noreply.github.com>
2025-05-14 00:43:29 -0500

    remove default HttpError message

--- src/common/errors/HttpError.ts
+++ src/common/errors/HttpError.ts
@@ -1,5 +1,5 @@
 export class HttpError extends Error {
-  constructor(public status: number, public message: string = "Error") {
+  constructor(public status: number, public message: string) {
     super(message);
     this.name = this.constructor.name;
     Error.captureStackTrace(this, this.constructor);
