033c4e12166553d07be0aeebe2fe1da11544d6eb
David <102764748+davidbutsch@users.noreply.github.com>
2025-05-16 15:51:58 -0500

    log all requests

--- src/libs/express.ts
+++ src/libs/express.ts
@@ -17,6 +17,16 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+// Log requests
+app.use((request, _response, next) => {
+  Logger.info({
+    message: `${request.method} ${request.url}`,
+    url: request.url,
+    headers: request.headers,
+  });
+  next();
+});
+
 // Routes
 app.use("/customers", customerRouter);
 app.use("/products", productRouter);
@@ -24,8 +34,8 @@ app.use("/prices", priceRouter);
 app.use("/subscriptions", subscriptionRouter);
 
 // Handle undefined routes
-app.use((_request, _response, next) =>
-  next(new NotFoundError("Route not found."))
+app.use((request, _response, next) =>
+  next(new NotFoundError(`${request.method} ${request.url} not found.`))
 );
 
 app.use(errorHandler);
