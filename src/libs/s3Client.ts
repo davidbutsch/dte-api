3a686a01197a61cef4d8f6ec0a48f30bde50760d
David <102764748+davidbutsch@users.noreply.github.com>
2025-06-08 20:55:08 -0500

    change aws region to us-east-2 (ohio)

--- src/libs/s3Client.ts
+++ src/libs/s3Client.ts
@@ -2,7 +2,7 @@ import { env } from "@/common";
 import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3Client = new S3Client({
-  region: "us-west-2",
+  region: "us-east-2",
   credentials: {
     accessKeyId: env.keys.ACCESS_KEY_ID,
     secretAccessKey: env.keys.SECRET_ACCESS_KEY,
